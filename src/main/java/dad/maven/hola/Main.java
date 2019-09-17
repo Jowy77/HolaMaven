c2abb9d802dec07bc0f4cafebe138c79e18ea19e
Jowy77 <Iproyectz@hotmail.com>
2019-09-17 12:25:55 +0100

    first commit

---
+++ src/main/java/dad/maven/hola/Main.java
@@ -0,0 +1,14 @@
+package dad.maven.hola;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String greetings = org.apache.commons.lang3.StringUtils.reverse("Hola Maven");
+		
+		System.out.println(greetings);
+
+	}
+
+}
